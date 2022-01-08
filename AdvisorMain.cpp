64ba07eb732cebdbcd954e19b0ce4bdb13b77d78
hadarsharon <hadars@crazylabs.com>
2022-01-08 15:28:32 +0200

    Added skeletons for all necessary commands

--- AdvisorMain.cpp
+++ AdvisorMain.cpp
@@ -17,10 +17,34 @@ void AdvisorMain::printHelp() {
     std::cout << BOTPROMPT << "The available commands are help, help <cmd>, avg, time, ..." << std::endl;
 }
 
-void AdvisorMain::helpCmd(const std::string& cmd) {
+void AdvisorMain::helpCmd(const std::string &cmd) {
 
 }
 
 void AdvisorMain::listProducts() {
+
+}
+
+double AdvisorMain::getProductMinOfType(std::string product, std::string type) {
+    return 0;
+}
+
+double AdvisorMain::getProductMaxOfType(std::string product, std::string type) {
+    return 0;
+}
+
+double AdvisorMain::getProductAvgOfTypeOverTimesteps(std::string product, std::string type, int timesteps) {
+    return 0;
+}
+
+double AdvisorMain::predictProductNextMaxMinOfType(bool max_or_min, std::string product, std::string type) {
+    return 0;
+}
+
+void AdvisorMain::printTime() {
+
+}
+
+void AdvisorMain::moveToNextTimestep() {
 
 }
--- AdvisorMain.h
+++ AdvisorMain.h
@@ -17,14 +17,34 @@ public:
 private:
     void printMenu();
 
-    /** List all available commands */
+    /** C1: help - List all available commands */
     void printHelp();
 
-    /** Output help for the specified command */
-    void helpCmd(const std::string& cmd);
+    /** C2: help cmd - Output help for the specified command */
+    void helpCmd(const std::string &cmd);
 
-    /** List available products */
+    /** C3: prod - List available products */
     void listProducts();
+
+    /** C4: min - Find minimum bid or ask for product in current time step */
+    double getProductMinOfType(std::string product, std::string type);  // TODO: use Enum for type
+
+    /** C5: max - Find maximum bid or ask for product in current time step */
+    double getProductMaxOfType(std::string product, std::string type);  // TODO: use Enum for type
+
+    /** C6: avg - compute average ask or bid for the sent product over the sent number of time steps  */
+    // TODO: verify type of timesteps, use Enum for type
+    double getProductAvgOfTypeOverTimesteps(std::string product, std::string type, int timesteps);
+
+    /** C7: predict - predict max or min ask or bid for the sent product for the next time step */
+    // TODO: use Enum for type and perhaps also max or min
+    double predictProductNextMaxMinOfType(bool max_or_min, std::string product, std::string type);
+
+    /** C8: time - state current time in dataset, i.e. which timeframe are we looking at */
+    void printTime();
+
+    /** C9: step - move to next time step */
+    void moveToNextTimestep();
 };
 
 
