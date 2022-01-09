757a12e03b68c3a31de26b3f1b0dfd57e1912cd4
hadarsharon <hadars@crazylabs.com>
2022-01-09 21:07:40 +0200

    Added comments to CSVReadher.h

--- CSVReader.h
+++ CSVReader.h
@@ -10,11 +10,14 @@ class CSVReader {
 public:
     CSVReader();
 
+    /** Given a CSV data file, read the data and convert the records into a vector of OrderBookEntry objects */
     static std::vector<OrderBookEntry> readCSV(const std::string &csvFile);
 
+    /** Function to tokenise each CSV line (i.e. record) into a vector */
     static std::vector<std::string> tokenise(const std::string &csvLine, char separator);
 
 private:
+    /** private utility function to aid in the conversion of raw CSV rows to OrderBookEntry objects */
     static OrderBookEntry stringsToOBE(std::vector<std::string> tokens);
 };
 
